2d14f204c7f66f73da13212a04ca202d45176cf1
AFI25 <afismail.25@gmail.com>
2022-11-21 21:40:17 -0500

    Added javascript

---
+++ script.js
@@ -0,0 +1,28 @@
+let emailElement = document.querySelector('#email');
+let messageElement = document.querySelector('#message');
+let submitButton = document.querySelector('#submit-button');
+
+submitButton.addEventListener('click', function(event) {
+	event.preventDefault();
+
+	let emailValue = emailElement.value;
+	let messageValue = messageElement.value;
+
+	if(emailValue.includes('@')){
+		alert('Thank you for your message')
+
+
+
+		
+	}else{
+		alert('Oh no it looks like thats an invalid email address! Please tr again.')
+	}
+	
+})
+
+//selecting html elements
+//attaching 'click' listener
+//getting user entered values
+//javascript validations
+
+
